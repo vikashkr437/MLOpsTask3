e8c5100ac131086958b4bae507c4351659f5e436
vikashkr437 <62397833+vikashkr437@users.noreply.github.com>
2020-06-02 04:35:58 +0530

    program to create model now customized

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
 
 
 from keras.datasets import mnist
@@ -15,25 +14,39 @@ dataset = mnist.load_data('mymnist.npz')
 train , test = dataset
 X_train , y_train = train
 X_test , y_test = test
+L=28
+W=28
+D=L*W
+no_of_matrix=512
 
 
-X_train_1d = X_train.reshape(-1 , 28*28)
-X_test_1d = X_test.reshape(-1 , 28*28)
+X_train_1d = X_train.reshape(-1 , D)
+X_test_1d = X_test.reshape(-1 , D)
 X_train = X_train_1d.astype('float32')
 X_test = X_test_1d.astype('float32')
 y_train_cat = to_categorical(y_train)
 y_test_cat = to_categorical(y_test)
 
 
-model = Sequential()
-model.add(Dense(units=512, input_dim=28*28, activation='relu'))
-#model.summary()
-model.add(Dense(units=256, activation='relu'))
-#model.add(Dense(units=128, activation='relu'))
-#model.add(Dense(units=128, activation='relu'))
-model.add(Dense(units=64, activation='relu'))
-model.add(Dense(units=32, activation='relu'))
-model.add(Dense(units=10, activation='softmax'))
+def build_layers(lyr, repeat):
+	model = Sequential()
+	model.add(Dense(units=no_of_matrix, input_dim=D, activation='relu'))
+	p=1
+	for i in range(0,lyr):
+		p=p*2
+		model.add(Dense(units=no_of_matrix//p, activation='relu'))
+		if repeat!=0 :
+			model.add(Dense(units=no_of_matrix//p, activation='relu'))
+			repeat-=1
+	model.add(Dense(units=10, activation='softmax'))
+	return model
+file1=open("layers.txt","r")
+file2=open("repeats.txt","r")
+file3=open("accuracy.txt","w")
+repeat=int(file2.read())
+lyr=int(file1.read())
+
+model=build_layers(lyr,repeat)
 model.compile(optimizer= Adam(learning_rate=0.0001), loss='categorical_crossentropy', 
              metrics=['accuracy']
              )
@@ -42,10 +55,11 @@ h = model.fit(X_train, y_train_cat, epochs=5,verbose=0)
 model.save("MyMNIST.h5")
 score = model.evaluate(X_test,y_test_cat,verbose=0)
 accuracy = score[1]
-accuracy
+file3.write(str(accuracy))
+file1.close()
+file2.close()
+file3.close()
 
-
-# In[ ]:
 
 
 
