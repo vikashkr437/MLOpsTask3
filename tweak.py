2bfc9958469b68423432d72ffefcbcc681bf5559
vikashkr437 <62397833+vikashkr437@users.noreply.github.com>
2020-06-02 12:12:13 +0530

    code to tweak the model structure

--- tweak.py
+++ tweak.py
@@ -1,27 +1,27 @@
-file1=open("iterations.txt","r")
+file1=open("/root/workspace/iterations.txt","r")
 i=int(file1.read())
 file1.close()
 
-file1=open("layers.txt","r")
+file1=open("/root/workspace/layers.txt","r")
 l=int(file.read())
 file1.close()
 
-file1=open("repeats.txt","r")
+file1=open("/root/workspace/repeats.txt","r")
 r=int(file1.read())
 file1.close()
 
 if i < 3 :
 	l+=1
-	file1=open("layers.txt","w")
+	file1=open("/root/workspace/layers.txt","w")
 	file1.write(str(l))
 	file1.close()
 else :
 	r+=1
-	file1=open("repeats.txt","w")
+	file1=open("/root/workspace/repeats.txt","w")
 	file1.write(str(r))
 	file1.close()
 
 i+=1
-file1=open("iterations.txt","w")
+file1=open("/root/workspace/iterations.txt","w")
 file1.write(str(i))
 file1.close()
